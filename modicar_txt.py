a422ef1f561cf199dee786527986919864490ca9
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-13 15:48:27 -0300

    Update modicar_txt.py

--- modicar_txt.py
+++ modicar_txt.py
@@ -1,18 +1,32 @@
-valores_celulares = [850, 2230, 1500, 3500, 5000]
 
-'''
+# valores_celulares = [850, 2230, 1500, 3500, 5000]
+# utf8 = "utf-8"
+
+# '''
  
- "r"  -> Usado somente para ler algo
- "w"  -> Usado somente para escrever algo
- "r+" -> Usado para ler e escrever algo
- "a"  -> Usado para acrescentar algo
+#  "r"  -> Usado somente para ler algo
+#  "w"  -> Usado somente para escrever algo OBS.: sobrepõe as informações antigas
+#  "r+" -> Usado para ler e escrever algo
+#  "a"  -> Usado para acrescentar algo OBS.: acrescenta sem sobrepor
 
-'''
+# '''
 
 # # COM "r"
 # with open("arquivos_txt/valores_celulares.txt", "r") as arquivo: # se nn existir o arquivo, ele será criado
 #     for valor in arquivo:
 #         print(valor)
+# # read -> arquivos simples (ex.: senhas, tokens, informações únicas)
+# with open("arquivos_txt/email.txt", "r") as arquivo:
+#     email = arquivo.read()
+# print(email)
+
+# # readline -> para arquivos maiores
+# with open("arquivos_txt/mensagem.txt", "r", encoding = utf8) as arquivo: # encoding = "utf-8" é para aceitar caracteres especiais
+#     mensagem = arquivo.readlines() # tranforma as linhas do arquivo em uma lista 
+# print(mensagem)
+# for linha in mensagem:
+#     if "faturamento" in linha:
+#         print(linha)
 
 # # COM "w"
 # with open("arquivos_txt/valores_celulares.txt", "w") as arquivo: 
